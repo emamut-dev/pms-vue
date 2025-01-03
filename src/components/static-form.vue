9eaff5ed966dc60e7436203a7230d201384dea2b
Faber Vergara <fabervergara@gmail.com>
2025-01-02 21:19:28 -0500

    fix: static form layout

--- src/components/static-form.vue
+++ src/components/static-form.vue
@@ -2,44 +2,46 @@
   <form>
     <div class="row mt-4">
       <div class="col">
-        <div class="form-floating">
+        <div class="input-group">
+          <span class="input-group-text" id="background-url"><IconLayersSelectedBottom /></span>
           <input
             type="url"
             class="form-control"
             id="img-fondo"
-            aria-describedby="nameHelp"
-            placeholder="https://example.com/image.jpg"
+            aria-describedby="background-url"
+            placeholder="URL imagen de fondo"
           />
-          <label for="img-fondo">URL imagen de fondo</label>
-          <div class="form-text">Ejemplo: https://www.example.com/image.png</div>
         </div>
+        <div class="form-text">Ejemplo: https://www.example.com/image.png</div>
       </div>
       <div class="col">
-        <div class="form-floating">
+        <div class="input-group">
+          <span class="input-group-text" id="front-url"><IconLayersSelected /></span>
           <input
             type="url"
             class="form-control"
             id="img-superior"
-            aria-describedby="nameHelp"
-            placeholder="https://example.com/image.jpg"
+            aria-describedby="front-url"
+            placeholder="URL imagen superior"
           />
-          <label for="img-fondo">URL imagen superior</label>
           <div class="form-text">Ejemplo: https://www.example.com/image.png</div>
         </div>
       </div>
       <div class="col-md-2 text-center">
-        <button class="btn btn-success btn-lg mt-1">Exportar <IconCloudDownload /></button>
+        <button class="btn btn-success">Exportar <IconCloudDownload /></button>
       </div>
     </div>
   </form>
 </template>
 
 <script>
-import { IconCloudDownload } from '@tabler/icons-vue'
+import { IconCloudDownload, IconLayersSelectedBottom, IconLayersSelected } from '@tabler/icons-vue'
 
 export default {
   components: {
     IconCloudDownload,
+    IconLayersSelectedBottom,
+    IconLayersSelected,
   },
 }
 </script>
