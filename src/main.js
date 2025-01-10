7ecff319bbb88bdd7ff504ac3402a922b74b6730
Faber Vergara <fabervergara@gmail.com>
2025-01-10 14:10:22 -0500

    feat: install vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router from './router/'
 import App from './App.vue'
 
 // Bootstrap
@@ -18,4 +19,5 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
+app.use(router)
 app.mount('#app')
