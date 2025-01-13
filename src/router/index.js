c80087f203ee50d11f297013f80682e128866f1e
Faber Vergara <fabervergara@gmail.com>
2025-01-13 17:08:52 -0500

    fix: fix page reload

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '../views/HomePage.vue'
-import ProfileOne from '../views/ProfileOneView.vue'
 import ProfileOneView from '../views/ProfileOneView.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/profile-one', component: ProfileOneView },
+  { path: 'profile-one', component: ProfileOneView },
 ]
 
 const router = createRouter({
